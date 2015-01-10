0fa7e6081a287354345dc66d2d7d0b2c35f82ca0
hawk_hsieh <hawk_hsieh@miiicasa.com>
2015-01-11 01:24:59 +0800

    Remove beego src

--- src/beego/runlocal.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-thisDir=`echo $(cd $(dirname $0);pwd)`
-moveTarget=`(echo $GOPATH/src/ | sed 's,//,/,g')`
-cd $moveTarget
-rm beego
-ln -s $thisDir beego
-cd $thisDir
-bee run beego
